fab802adcd96a047f3d803daa04db69a27947cbd
3u13r2 <leonard.cohnen+github2@gmail.com>
2026-09-10 02:28:05 +0200

    Update main.go

--- cmd/tetragon/main.go
+++ cmd/tetragon/main.go
@@ -771,7 +771,7 @@ func serveOne(
 	register(grpcServer)
 
 	go func() {
-		log.Info("Starting gRPC server", "protocol", proto, "address", addr)
+		log.Info("Starting gRPC server hello world", "protocol", proto, "address", addr)
 		if err := grpcServer.Serve(listener); err != nil {
 			log.Error("gRPC Serve returned", logfields.Error, err)
 		}
